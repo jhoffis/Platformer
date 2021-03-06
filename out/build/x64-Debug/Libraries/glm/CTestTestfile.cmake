e99ae27bf238e3b77743eefe864efb7cc92cb6e6
jhoffis <jonahoffis@gmail.com>
2021-03-06 17:06:54 +0100

    fjernet out mappe

--- out/build/x64-Debug/Libraries/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jh/source/repos/Platformer/Libraries/glm
-# Build directory: C:/Users/jh/source/repos/Platformer/out/build/x64-Debug/Libraries/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- out/build/x64-Debug/Libraries/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jh/source/repos/Platformer/Libraries/glm/glm
-# Build directory: C:/Users/jh/source/repos/Platformer/out/build/x64-Debug/Libraries/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
